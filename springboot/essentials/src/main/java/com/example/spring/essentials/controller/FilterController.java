42cd15472bae6c93bd99ecc78d505a4ea12cee0a
BJH <imganchi@gmail.com>
2025-02-27 10:52:58 +0900

    filter

--- springboot/essentials/src/main/java/com/example/spring/essentials/controller/FilterController.java
+++ springboot/essentials/src/main/java/com/example/spring/essentials/controller/FilterController.java
@@ -1,4 +1,14 @@
 package com.example.spring.essentials.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class FilterController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        System.out.println("[hello]");
+        return "hello";
+    }
 }
