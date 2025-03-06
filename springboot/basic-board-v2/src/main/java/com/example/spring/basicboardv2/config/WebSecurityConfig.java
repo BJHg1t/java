5fb36d2a7e168c4ab16c4c82c63311c2df1b4a7c
BJH <imganchi@gmail.com>
2025-03-06 11:56:57 +0900

    login

--- springboot/basic-board-v2/src/main/java/com/example/spring/basicboardv2/config/WebSecurityConfig.java
+++ springboot/basic-board-v2/src/main/java/com/example/spring/basicboardv2/config/WebSecurityConfig.java
@@ -4,6 +4,8 @@ import com.example.spring.basicboardv2.config.filter.TokenAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -11,6 +13,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @RequiredArgsConstructor
@@ -35,13 +38,28 @@ public class WebSecurityConfig {
                 .sessionManagement(
                         session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 세션을 비활성화, 로그인 정보를 쿠키게 담기 위해서
                 )
+                .authorizeHttpRequests(
+                        auth -> auth
+                                .requestMatchers(
+                                        new AntPathRequestMatcher("/member/join", "GET"),
+                                        new AntPathRequestMatcher("/member/login", "GET"),
+                                        new AntPathRequestMatcher("/join", "POST"),
+                                        new AntPathRequestMatcher("/login", "POST")
+                                )
+                                .permitAll()
+                                .anyRequest().authenticated()
+                )
                 .logout(AbstractHttpConfigurer::disable)
                 // JWT 필터 추가
                 .addFilterBefore(tokenAuthenticationFilter, UsernamePasswordAuthenticationFilter.class) // 시큐리티의 인증 필터보다 앞에 설정해서 인증을 더 할 필요없게 만듬 > 로그인 하기 전에
-
         ;
 
         return http.build();
+    }
+
+    @Bean // 인증을 진행하는 manager 반환 > 인증을 진행한다 = 로그인 한다
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
     }
 
     @Bean
--- springboot/basic-board-v2/src/main/resources/static/js/signIn.js
+++ springboot/basic-board-v2/src/main/resources/static/js/signIn.js
@@ -1,5 +1,31 @@
 $(document).ready(() => {
 
+    $('#signin').click(() => {
 
+        let userId = $('#user_id').val();
+        let password = $('#password').val();
+
+        let formData = {
+            username : userId,
+            password : password
+        }
+
+        $.ajax({
+            type: 'POST',
+            url: '/login',
+            data: JSON.stringify(formData), // 데이터를 JSON 형식으로 변환
+            contentType: 'application/json; charset=urf-8', // 전송 데이터의 타입, json으로 보내겠다 알려주는 것
+            dataType: 'json', // 서버에서 받을 데이터의 타입
+            success: (response) => {
+                console.log(response);
+                alert('로그인이 성공했습니다.');
+            },
+            error: (error) => {
+                console.log('오류발생 : ', error);
+                alert('로그인 중 오류가 발생했습니다.');
+            }
+        });
+
+    });
 
 });
