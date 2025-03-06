f827f93c95e3aa9c723e86507756939ea8b69067
BJH <imganchi@gmail.com>
2025-03-06 10:54:57 +0900

    sign up

--- springboot/basic-board-v2/src/main/java/com/example/spring/basicboardv2/controller/MemberApiController.java
+++ springboot/basic-board-v2/src/main/java/com/example/spring/basicboardv2/controller/MemberApiController.java
@@ -1,10 +1,27 @@
 package com.example.spring.basicboardv2.controller;
 
+import com.example.spring.basicboardv2.dto.SignUpRequestDTO;
+import com.example.spring.basicboardv2.dto.SignUpResponseDTO;
+import com.example.spring.basicboardv2.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class MemberApiController {
 
+    private final MemberService memberService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-
+    @PostMapping("/join")
+    public SignUpResponseDTO signUp(@RequestBody SignUpRequestDTO signUpRequestDTO) {
+        System.out.println(signUpRequestDTO);
+        memberService.signUp(signUpRequestDTO.toMember(bCryptPasswordEncoder));
+        return SignUpResponseDTO.builder()
+                .successed(true) // 원래는 if ~ 예외 처리 해야 함
+                .build();
+    }
 }
