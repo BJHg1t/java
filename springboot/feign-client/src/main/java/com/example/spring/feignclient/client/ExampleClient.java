bc8799b2049a37a50e8ef9be589d76ca276acd2d
BJH <imganchi@gmail.com>
2025-03-11 01:20:10 +0900

    feign client weather

--- springboot/feign-client/src/main/java/com/example/spring/feignclient/client/ExampleClient.java
+++ springboot/feign-client/src/main/java/com/example/spring/feignclient/client/ExampleClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 @FeignClient(name = "exampleClient", url = "${feign-data.url}")
 // OpenFeign 라이브러리를 통해 HTTP 클라이언트를 정의하는 어노테이션
 // url에 직접 url을 입력해도 됨
-// FeignClient가 interface의 구현체를 구현해 놓았음
+// FeignClient가 interface의 구현체를 구현해 놓음
 public interface ExampleClient {
 
     // GET 요청 (데이터 조회)
