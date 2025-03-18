4b9e756176f0e1bd31acfc1a6b609ad2cc9e8b1d
BJH <imganchi@gmail.com>
2025-03-18 09:52:10 +0900

    catalog-service

---
+++ polar-service/catalog-service/src/main/java/com/example/spring/catalogservice/persistence/InMemoryBookRepository.java
@@ -0,0 +1,41 @@
+package com.example.spring.catalogservice.persistence;
+
+import com.example.spring.catalogservice.domain.Book;
+import com.example.spring.catalogservice.domain.BookRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Repository
+public class InMemoryBookRepository implements BookRepository {
+
+    // 메모리를 사용 > 휘발성이지만 빠름, xml에 해당하는 부분
+    private static final Map<String, Book> books = new ConcurrentHashMap<>();
+
+    @Override
+    public Iterable<Book> findAll() {
+        return books.values();
+    }
+
+    @Override
+    public Optional<Book> findByIsbn(String isbn) {
+        return existsByIsbn(isbn) ? Optional.of(books.get(isbn)) : Optional.empty();
+    }
+
+    @Override
+    public Book save(Book book) {
+        return books.put(book.isbn(), book);
+    }
+
+    @Override
+    public void deleteByIsbn(String isbn) {
+        books.remove(isbn);
+    }
+
+    @Override
+    public boolean existsByIsbn(String isbn) {
+        return books.containsKey(isbn);
+    }
+}
