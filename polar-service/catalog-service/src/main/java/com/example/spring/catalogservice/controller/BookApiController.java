90a994d77177f47f14e7fdc6ccdd84278a9e41f3
BJH <imganchi@gmail.com>
2025-03-18 10:08:57 +0900

    catalog-service

--- polar-service/catalog-service/src/main/java/com/example/spring/catalogservice/controller/BookApiController.java
+++ polar-service/catalog-service/src/main/java/com/example/spring/catalogservice/controller/BookApiController.java
@@ -3,9 +3,7 @@ package com.example.spring.catalogservice.controller;
 import com.example.spring.catalogservice.domain.Book;
 import com.example.spring.catalogservice.service.BookService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,5 +15,10 @@ public class BookApiController {
     @GetMapping
     public Iterable<Book> getBooks() {
         return bookService.viewBookList();
+    }
+
+    @PostMapping // 프로젝트 만들 때 domain 대신 DTO 만들어서 사용할 것
+    public Book addBook(@RequestBody Book book) {
+        return bookService.addBookToCatalog(book);
     }
 }
