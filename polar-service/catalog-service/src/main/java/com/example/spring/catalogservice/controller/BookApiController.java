1cd208e6bec49e0cc72bafd9f92309877958a436
BJH <imganchi@gmail.com>
2025-03-18 15:20:14 +0900

    catalog-service

--- polar-service/catalog-service/src/main/java/com/example/spring/catalogservice/controller/BookApiController.java
+++ polar-service/catalog-service/src/main/java/com/example/spring/catalogservice/controller/BookApiController.java
@@ -2,6 +2,7 @@ package com.example.spring.catalogservice.controller;
 
 import com.example.spring.catalogservice.domain.Book;
 import com.example.spring.catalogservice.service.BookService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,8 +18,24 @@ public class BookApiController {
         return bookService.viewBookList();
     }
 
+    @GetMapping("/{isbn}")
+    public Book getBook(@PathVariable String isbn) {
+        return bookService.viewBook(isbn);
+    }
+
     @PostMapping // 프로젝트 만들 때 domain 대신 DTO 만들어서 사용할 것
-    public Book addBook(@RequestBody Book book) {
+    public Book addBook(@Valid @RequestBody Book book) {
         return bookService.addBookToCatalog(book);
     }
+
+    @PutMapping("/{isbn}")
+    public Book updateBook(@PathVariable String isbn, @Valid @RequestBody Book book) {
+        return bookService.editBookDetails(isbn, book);
+    }
+
+    @DeleteMapping("/{isbn}")
+    public void deleteBook(@PathVariable String isbn) {
+        bookService.removeBookFromCatalog(isbn);
+    }
+
 }
