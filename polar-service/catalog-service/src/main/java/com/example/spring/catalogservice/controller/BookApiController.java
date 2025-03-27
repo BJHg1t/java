55d0fb826ec25371984c9a98e92658e20bc9b046
BJH <imganchi@gmail.com>
2025-03-27 16:56:57 +0900

    logs

--- polar-service/catalog-service/src/main/java/com/example/spring/catalogservice/controller/BookApiController.java
+++ polar-service/catalog-service/src/main/java/com/example/spring/catalogservice/controller/BookApiController.java
@@ -4,8 +4,10 @@ import com.example.spring.catalogservice.domain.Book;
 import com.example.spring.catalogservice.service.BookService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/books")
@@ -20,6 +22,7 @@ public class BookApiController {
 
     @GetMapping("/{isbn}")
     public Book getBook(@PathVariable String isbn) {
+        log.info("getBook: {}", isbn); // order-service에서 책 목록을 받아오지 못해서 postman으로 요청보내서 로그 계속 찍어보려고 사용
         return bookService.viewBook(isbn);
     }
 
